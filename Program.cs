a5d9f25e370c5c524602e713b189ae19ca7ed040
Nguyễn Phước Hoàng Long <long.nph.60cntt@ntu.edu.vn>
2020-12-30 16:20:24 +0700

    code mot phan

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TTCS_THUAT_TOAN_TIM_DUONG_DI_NGAN_NHAT
 {
@@ -10,24 +11,8 @@ namespace TTCS_THUAT_TOAN_TIM_DUONG_DI_NGAN_NHAT
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
-
-            Console.WriteLine("Nhập vào a: ");
-            float a = float.Parse(Console.ReadLine());
-
-            if (a == 0)
-            {
-                Console.WriteLine("a phải khác 0");
-            }
-            else
-            {
-                Console.WriteLine("Nhập vào b: ");
-                float b = float.Parse(Console.ReadLine());
-
-                float x = -b / a;
-                Console.WriteLine("{0}x + {1} = 0 \n=> x = {2}", a, b, x);
-            }
-            Console.ReadKey();
+            ttdijktra tt = new dijktra();
+            tt.dijktra(1,6);
         }
     }
 }

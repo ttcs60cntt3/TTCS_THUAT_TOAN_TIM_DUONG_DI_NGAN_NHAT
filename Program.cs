fd0767097c12b1fabe8e87b98fbdf744d27725e9
Nguyễn Phước Hoàng Long <long.nph.60cntt@ntu.edu.vn>
2021-01-08 08:29:36 +0700

    push

--- Program.cs
+++ Program.cs
@@ -4,10 +4,65 @@ namespace TTCS_THUAT_TOAN_TIM_DUONG_DI_NGAN_NHAT
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-           TimDuongDijktra tt = new TimDuongDijktra();
-            tt.TimDuong(1, 6);
+            int[,] graph =
+            {
+             { 0, 1, 0, 1, 0, 0 },
+             { 1, 0, 1, 0, 0, 0 },
+             { 0, 1, 0, 1, 3, 0 },
+             { 1, 0, 1, 0, 0, 0 },
+             { 0, 0, 3, 0, 0, 1 },
+             { 0, 0, 0, 0, 1, 0 }
+            
+            };
+            Dijkstra(graph, 0,6);
+            Console.ReadKey();
+        }
+        private static int KhoangCachMin(int[] khoangcach, bool[] duongdingannhat, int sodinh)
+        {
+            int min = int.MaxValue;
+            int minIndex = 0;
+            for (int v = 0; v < sodinh; ++v)
+            {
+                if (duongdingannhat[v] == false && khoangcach[v] <= min)
+                {
+                    min = khoangcach[v];
+                    minIndex = v;
+                }
+            }
+            return minIndex;
+        }
+        private static void Print(int[] khoangcach, int sodinh)
+        {
+            Console.WriteLine("khoang cach tu dinh nguon");
+
+            for (int i = 0; i < sodinh; ++i)
+                Console.WriteLine("{0}\t  {1}", i, khoangcach[i]);
+        }
+        public static void Dijkstra(int[,] graph, int dinhnguon, int sodinh)
+        {
+            int[] khoangcach = new int[sodinh];
+            bool[] duongdingannhat = new bool[sodinh];
+
+            for (int i = 0; i < sodinh; ++i)
+            {
+                khoangcach[i] = int.MaxValue;
+                duongdingannhat[i] = false;
+            }
+            khoangcach[dinhnguon] = 0;
+
+            for (int count = 0; count < sodinh - 1; ++count)
+            {
+                int u = KhoangCachMin(khoangcach, duongdingannhat, sodinh);
+                duongdingannhat[u] = true;
+
+                for (int v = 0; v < sodinh; ++v)
+                    if (!duongdingannhat[v] && Convert.ToBoolean(graph[u, v])
+                && khoangcach[u] != int.MaxValue && khoangcach[u] + graph[u, v] < khoangcach[v])
+                        khoangcach[v] = khoangcach[u] + graph[u, v];
+            }
+            Print(khoangcach, sodinh);
         }
     }
 }

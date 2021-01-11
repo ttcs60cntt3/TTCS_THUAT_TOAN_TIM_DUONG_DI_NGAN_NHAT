f4960168a33dae67d4343a1f86c3c035112ccdd9
Dương Ngọc Thoại <76215855+ttcs60cntt3@users.noreply.github.com>
2021-01-11 08:49:53 +0700

    change OOP

--- Bellman_OOP.cs
+++ Bellman_OOP.cs
@@ -19,28 +19,34 @@ namespace BellmanFord
         }
     }
 
-    class DoThi
+    class Graph
     {
         List<string> dsDinh;
-        List<Canh> dsCanh;
-        string nguon;
-        
-        public DoThi(string path)
+        private List<Canh> dsCanh;
+        private string nguon;
+        string path;
+
+        public List<string> DsDinh { get => dsDinh; set => dsDinh = value; }
+        internal List<Canh> DsCanh { get => dsCanh; set => dsCanh = value; }
+        public string Nguon { get => nguon; set => nguon = value; }
+
+        public Graph(string p="")
         {
+            path = p;
             string[] data = System.IO.File.ReadAllLines(path);
             dsCanh = new List<Canh>();
             dsDinh = new List<string>();
-            for(int i = 0; i < data.Length; i++)
+            for (int i = 0; i < data.Length; i++)
             {
                 string[] s = data[i].Split(' ');
                 dsCanh.Add(new Canh(s[0], s[1], s[2]));
-                if(!dsDinh.Contains(s[0]))
+                if (!dsDinh.Contains(s[0]))
                     dsDinh.Add(s[0]);
                 if (!dsDinh.Contains(s[1]))
                     dsDinh.Add(s[1]);
             }
             Console.WriteLine("Danh sach cac dinh: ");
-            for(int i = 0; i < dsDinh.Count; i++)
+            for (int i = 0; i < dsDinh.Count; i++)
             {
                 Console.Write(dsDinh[i] + " ");
             }
@@ -51,9 +57,12 @@ namespace BellmanFord
         {
             return "Dinh nguon la: " + nguon;
         }
-
-        public void BellmanFord()
+    }
+    public Bellman(string p = " ") : base(p)
         {
+        }
+        public void BellmanFord()
+        { 
             //distance[], predecessor[]
             Dictionary<string, int> distance = new Dictionary<string,int>();
             Dictionary<string, string> predecessor = new Dictionary<string,string>();
@@ -64,23 +73,23 @@ namespace BellmanFord
                     predecessor[v] = inf        // Và không có bất kì đỉnh tiền nhiệm nào
              */
             //Xác định inf (vô cùng)
-            int inf = dsCanh.Max(p => p.cost) + 100;
-            foreach(string v in dsDinh)
+            int inf = DsCanh.Max(p => p.cost) + 100;
+            foreach(string v in DsDinh)
             {
                 distance.Add(v, inf);
                 predecessor.Add(v, null);
             }
             // distance[source] = 0                   // Diem xuat luon co do lon bang 0
-            distance[nguon] = 0;
+            distance[Nguon] = 0;
             /* bước 2: kết nạp cạnh
                 for i from 1 to size(danh_sách_đỉnh):       
                 for each(u, v) in danh_sách_cung:
                 if khoảng_cách(v) > khoảng_cách(u) + trọng_số(u, v):
                 khoảng_cách(v):= khoảng_cách(u) + trọng_số(u, v)
                 đỉnh_liền_trước(v):= u*/
-            for(int i = 0; i < dsDinh.Count -1; i++)
+            for(int i = 0; i < DsDinh.Count -1; i++)
             {
-                foreach(Canh c in dsCanh)
+                foreach(Canh c in DsCanh)
                 {
                     if(distance[c.u]+c.cost < distance[c.v])
                     {
@@ -93,7 +102,7 @@ namespace BellmanFord
             /*for each(u, v) in danh_sách_cung:
             if khoảng_cách(v) > khoảng_cách(u) + trọng_số(u, v):
             error "Đồ thị chứa chu trình âm"*/
-            foreach(Canh c in dsCanh)
+            foreach(Canh c in DsCanh)
             {
                 if(distance[c.u] + c.cost < distance[c.v])
                 {
@@ -102,17 +111,17 @@ namespace BellmanFord
             }
             // Trả vể độ lớn và các đỉnh nguồn
             //in kết quả
-            Console.WriteLine("Duong di ngan nhat tu dinh" + nguon + "la: ");
+            Console.WriteLine("Duong di ngan nhat tu dinh " + Nguon + " la: ");
             Console.WriteLine("--Khoang cach tu nguon den cac dinh: ");
             foreach(var item in distance)
             {
                 Console.WriteLine(item.Key + " " + item.Value);
             }
             Console.WriteLine("--Duong di giua cac dinh: ");
-            Console.WriteLine("Dinh\tTien nhiem");
+            Console.WriteLine("Dinh | Tien nhiem");
             foreach(var item in predecessor)
             {
-                Console.WriteLine(item.Key + "  " + item.Value);       
+                Console.WriteLine(item.Key + "\t" + item.Value);       
             }
 
         }
@@ -122,10 +131,11 @@ namespace BellmanFord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tim duong di ngan nhat Bellman Ford");
-            DoThi dt = new DoThi("a.txt");
+           Console.WriteLine("Tim duong di ngan nhat Bellman Ford");
+            Bellman dt = new Bellman("a.txt");
             dt.Print();
             dt.BellmanFord();
+            Console.ReadKey();
         }
     }
 }
